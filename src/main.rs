5e8bfa85bfbcd33931df075aefaef45a9ad08e68
felprangel <feliperangel897@gmail.com>
2025-03-19 20:36:44 -0300

    adiciona leitura do arquivo enviado

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
