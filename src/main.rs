b0ce835b79b030ab58bcb2e6435b0ee86696137d
felprangel <feliperangel897@gmail.com>
2025-03-19 20:19:10 -0300

    adiciona structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf
+}
+
 fn main() {
     let pattern = std::env::args().nth(1).expect("No pattern given");
     let path = std::env::args().nth(2).expect("No path given");
