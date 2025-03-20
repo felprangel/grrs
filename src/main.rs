a079cc4a2e2ed9f45d35320f96433ce63451bac6
felprangel <feliperangel897@gmail.com>
2025-03-19 21:24:24 -0300

    adiciona tratativa de erro

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
