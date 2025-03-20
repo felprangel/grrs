0c6405e2f8e08eff1dde6899bca3070c5c0e1faa
felprangel <feliperangel897@gmail.com>
2025-03-19 21:31:22 -0300

    adiciona erro personalizado

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ struct Cli {
     path: PathBuf
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<(), CustomError> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)?;
 
